f13788ce4a62795e4e52f2102692d01e9f427c48
Tate Brasel <tate@Shuksan.domain>
2019-08-05 13:36:56 -0700

    finding milepost 97 dataset

---
+++ local_scratch/tate-milepost97.R
@@ -0,0 +1,32 @@
+# Library setup
+library(MazamaSatelliteUtils)
+library(MazamaSpatialUtils)
+setSatelliteDataDir("~/Data/Satellite/milepost97fire")
+setSpatialDataDir("~/Data/Spatial")
+
+# Get bbox for Oregon
+loadSpatialData("USCensusStates")
+or <- subset(USCensusStates, stateCode == "OR")
+bbox_or <- sp::bbox(or)
+
+# Download files for 3pm on July 30, 2019
+# Pretty clear dark streak starting at Canyonville and extending south. Holes in
+# the very center.
+date <- lubridate::ymd_h("2019-07-31 16", tz = "UTC")
+paths <- goesaodc_downloadAOD(startdate = date)
+files <- goesaodc_listFiles(date)
+
+# Generate a frame for each file
+for (i in c(1:length(files))) {
+  nc <- goesaodc_openFile(filename = files[i])
+  
+  data <- goesaodc_createTibble(nc)
+  data_or <- dplyr::filter(data,
+                           lon > bbox_or["x", "min"], lon < bbox_or["x", "max"], 
+                           lat > bbox_or["y", "min"], lat < bbox_or["y", "max"])
+  
+  rstr <- goesaodc_createRaster(nc, bbox = bbox_or, res = 0.08)
+  raster::plot(rstr$AOD, main = paste("Milepost 97 AOD", i))
+  plot(or, add = TRUE)
+  points(x = c(-123.296895), y = c(42.858787), col = "red", cex = 3.0)
+}
