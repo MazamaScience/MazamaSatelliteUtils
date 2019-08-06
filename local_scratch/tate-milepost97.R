e32c447d9e629eb0ca0c42bf209f092009941d5d
Tate Brasel <tate@Shuksan.domain>
2019-08-06 14:47:09 -0700

    fixed grid size vignette

--- local_scratch/tate-milepost97.R
+++ local_scratch/tate-milepost97.R
@@ -9,14 +9,16 @@ loadSpatialData("USCensusStates")
 or <- subset(USCensusStates, stateCode == "OR")
 bbox_or <- sp::bbox(or)
 
-# Download files for 3pm on July 30, 2019
-# Pretty clear dark streak starting at Canyonville and extending south. Holes in
-# the very center.
+# Download files for 3pm on July 31, 2019
+# Pretty clear dark streak starting at Canyonville and extending south
 date <- lubridate::ymd_h("2019-07-31 16", tz = "UTC")
 paths <- goesaodc_downloadAOD(startdate = date)
 files <- goesaodc_listFiles(date)
 
-# Generate a frame for each file
+# Define AOD color palette
+pal_aod <- colorRampPalette(c("lightgoldenrod1", "red3"))
+
+# Plot a raster for each file
 for (i in c(1:length(files))) {
   nc <- goesaodc_openFile(filename = files[i])
   
@@ -26,7 +28,8 @@ for (i in c(1:length(files))) {
                            lat > bbox_or["y", "min"], lat < bbox_or["y", "max"])
   
   rstr <- goesaodc_createRaster(nc, bbox = bbox_or, res = 0.08)
-  raster::plot(rstr$AOD, main = paste("Milepost 97 AOD", i))
+  raster::plot(rstr$AOD, main = paste("Milepost 97 AOD", i), 
+               col = pal_aod(10), breaks = seq(-1.5, 2.5, by=0.4))
   plot(or, add = TRUE)
   points(x = c(-123.296895), y = c(42.858787), col = "red", cex = 3.0)
 }
