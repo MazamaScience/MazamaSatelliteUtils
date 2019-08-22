9d7c4a37290fc46e188f51845c66ae75abb79c89
Tate Brasel <tate@Shuksan.domain>
2019-08-22 15:01:55 -0700

    dqf boundary vignette

--- local_scratch/tate-daySnapshot.R
+++ local_scratch/tate-daySnapshot.R
@@ -1,8 +1,8 @@
-# ffmpeg -loglevel quiet -r 3 -f image2 -s 1280x720 -i %03d.png -vcodec libx264 -crf 25 ~/Desktop/USA_DQF0.mp4
-
 setSatelliteDataDir("~/Data/Satellite")
 
-dqfLevel <- 0
+dqfLevel <- 2
+frameRate <- 3
+outputDir <- "~/Desktop/"
 
 if (dqfLevel == 0) {
   dqfTitle <- "High"
@@ -27,19 +27,34 @@ for (i in 1:length(times)) {
   sp <- goesaodc_createSpatialPoints(nc, dqfLevel = dqfLevel)
   
   n <- stringr::str_pad(frameNumber, 3, 'left', '0')
-  frameFilePath <- paste0("~/Desktop/frames/", n, ".png")
+  frameFilePath <- paste0(tempdir(), "/", n, ".png")
   
   localTime <- times[i]
   attributes(localTime)$tzone = "America/Los_Angeles"
 
+  # Draw frame
   png(frameFilePath, width = 1280, height = 720, units = "px")
   maps::map("state")
   goesaodc_plotSpatialPoints(sp, cex = 0.4, add = TRUE)
+  
   title(paste0("Data Quality: ", dqfTitle,"\n", localTime, " PDT"), cex.main = 2.0)
-  mtext("No data for the NW beyond a certain radius", 
-        side = 1, line = 1.0, cex = 1.6)
+  if (dqfLevel < 2) {
+    mtext("No data for the NW beyond a certain radius", 
+          side = 1, line = 1.0, cex = 1.6)
+  }
   
   dev.off()
   
   frameNumber <- frameNumber + 1
 }
+
+# Create video from frames
+cmd_cd <- paste0("cd ", tempdir())
+cmd_ffmpeg <- paste0("ffmpeg -loglevel quiet -r ", 
+                     frameRate, " -f image2 -s 1280x720 -i ",
+                     "%03d.png -vcodec libx264 -crf 25 ", 
+                     outputDir, "USA_DQF", dqfLevel, ".mp4")
+cmd_rm <- paste0("rm *.png")
+cmd <- paste0(cmd_cd, " && ", cmd_ffmpeg)
+
+system(cmd)
