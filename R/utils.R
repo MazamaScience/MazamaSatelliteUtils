26e1f0c1c4d5060cac3c3b73041bca9db382d183
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2019-10-07 15:32:21 -0700

    Jon's minor refactoring.

--- R/utils.R
+++ R/utils.R
@@ -13,49 +13,66 @@
 #'
 #' @examples
 #' \donttest{
-#' # USE SAMPLE CODE FROM is_daylight.R
+#' # USE SAMPLE CODE FROM isDaylight.R
 #' }
 #' 
 
 bboxToVector <- function(
   bbox = NULL
 ) {
-  # SUPPORT FOR sp::bbox TYPE
+  
   if (
     class(bbox) == "matrix" &&
     all(colnames(bbox) == c("min", "max")) &&
     all(rownames(bbox) == c("x", "y"))
   ) {
+    
+    # Support for sp::bbox() type
     w <- bbox[1, 1]
     e <- bbox[1, 2]
     s <- bbox[2, 1]
     n <- bbox[2, 2]
-  # SUPPORT FOR raster::extent TYPE
+    
+    # Trust order and domain
+    
   } else if (
     class(bbox) == "Extent" &&
     typeof(bbox) == "S4"
   ) {
+    
+    # Support for raster::extent() type
     w <- bbox[1]
     e <- bbox[2]
     s <- bbox[3]
     n <- bbox[4]
-  # SUPPPORT FOR VECTOR OF FLOATS IN c(lonLo, lonHi, latLo, latHi) ORDER
+    
+    # Trust order and domain
+    
   } else if (
     is.numeric(bbox) &&
     is.vector(bbox)
   ) {
-    w <- bbox[1]
-    e <- bbox[2]
-    s <- bbox[3]
-    n <- bbox[4]
+    
+    # Support for vector of floats in c(lonLo, lonHi, latLo, latHi)
+    w <- min(bbox[1], bbox[2])
+    e <- max(bbox[1], bbox[2])
+    s <- min(bbox[3], bbox[4])
+    n <- max(bbox[3], bbox[4])
+    
+    if (w <= -180 || w >= 180 ||
+        e <= -180 || e >= 180)
+      stop("Longitude must be defined in -180:180")
+    
+    if (s <= -90 || s >= 90 ||
+        n <= -90 || n >= 90)
+      stop("Latitude must be defined in -90:90")
+    
   } else {
-    stop("bbox type not recognized")
+    
+    stop("Parameter 'bbox' is not recognized. Try c(W, E, S, N).")
+    
   }
-  lonLo <- w
-  lonHi <- e
-  latLo <- s
-  latHi <- n
-  # ADD SOME SANITY CHECKING HERE -180 <= lon <= 180, -90 <= lat <= 90
-  # Lo COORDS SHOULD BE LESS THAN Hi COORDS
-  return( c(lonLo, lonHi, latLo, latHi) )
+
+    return( c(w, e, s, n) )
+  
 }
