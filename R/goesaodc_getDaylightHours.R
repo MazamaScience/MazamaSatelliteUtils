7cfffa1275bfc65c05a3976a35b015497774c5a4
lagerratrobe <randre@gmail.com>
2019-10-25 13:57:45 -0700

    Adds goesaodc_getDaylightHours() function and tests to package.

--- R/goesaodc_getDaylightHours.R
+++ R/goesaodc_getDaylightHours.R
@@ -1,20 +1,38 @@
-#' @export 
-#' @title goesaodc_getDaylightHours
-#' @description Returns the sunrise and sunset hours for a location, timezone, 
-#' or bounding box.
-#' @param datetime Default: NULL
-#' @param latitude Default: NULL
-#' @param longitude Default: NULL
-#' @param timezone Default: NULL
-#' @param julian Default: FALSE
+#' @export
+#' 
+#' @title Calculates the sunrise and sunset hours for a specific date and 
+#' location
+#' 
+#' @description Returns the sunrise and sunset hours for a specific date and 
+#' location based on sun ephemerides calculations.  Location can be specified 
+#' via any of the following methods.
+#'
+#' \itemize{
+#' \item{}{Olson timezone, either as part of a POSIXt datetime, or specified 
+#' via the \code{timezone} parameter}
+#' \item{}{Geographic Bounding Box, either as a vector of floats in 
+#' \code{c(lonLo, lonHi, latLo,latHi)} order, or the return value from 
+#' \code{sp::bbox()}, or \code{raster::extent()} }
+#' \item{}{Longitude and Latitude specified in decimal degrees}
+#' }
+#' 
+#' @param datetime specific date for which daylight hours are requested
+#' @param latitude geographic latitude, in decimal degrees, for point of 
+#' interest
+#' @param longitude geographic longitude, in decimal degrees, for point of 
+#' interest
+#' @param timezone timezone in which to interpret the \code{datetime}
+#' @param bbox Bounding box for the region of interest
+#' @param julian logical, optional format to specify \code{datetime} parameter
 #' @return day_info List which contains sunrise and sunset times as POSIXt 
 #' values
+#' 
 #' @examples 
 #' \dontrun{
 #' library(MazamaSatelliteUtils)
 #' library(MazamaSpatialUtils)
 #' 
-#' EX 1. Get Sunrise and Sunset by Lat/Lon and Datetime - TO DO
+#' EX 1. Get Sunrise and Sunset by Lat/Lon and Datetime
 #' dayInfo <- goesaodc_getDaylightHours(
 #' datetime = "2019-09-06", 
 #' longitude = -123.245, 
@@ -25,13 +43,14 @@
 #' datetime = "2019-09-06",
 #' bbox = c(-124.566, -116.463, 41.991, 46.292)) # Oregon
 #' 
-#' EX 3. Get Sunrise and Sunset by Datetime and Timezone - TO DO
+#' EX 3. Get Sunrise and Sunset by Datetime and Timezone
 #' dayInfo <- goesaodc_getDaylightHours(
 #' datetime = "2019-09-06",
 #' timezone = "America/Los_Angeles")
 #' }
+#' 
 #' @rdname goesaodc_getDaylightHours
-
+#' 
 
 goesaodc_getDaylightHours <- function(
   datetime = NULL,
@@ -44,7 +63,33 @@ goesaodc_getDaylightHours <- function(
   
   MazamaCoreUtils::stopIfNull(datetime)
   
-  # ALL CALCULATIONS RELY ON THESE VARIABLES FOR BBOX-DERIVED EPHEMERIS
+  # Check whether both bbox and lat/lon exist. Only need one at start ----------
+  if ( !is.null(bbox) && !is.null(latitude) ) {
+    stop("Cannot process both BBOX and Lat/Lon at the same time. Pick one.")
+  }
+  
+  # ---- Check if a timezone has been passed in with a POSIXt ------------------
+  time_classes <- c("POSIXct", "POSIXt", "POSIXlt")
+  if ( class(datetime)[1] %in% time_classes ) {
+    timezone <- attr(datetime, "tzone")
+  }
+  
+  # If we only have a datetime and timezone, we need the timezone's bbox -------
+  if (is.null(bbox) && is.null(latitude) && !is.null(timezone)) {
+    param_tz <- timezone
+    tz <- subset(MazamaSpatialUtils::SimpleTimezones, timezone == param_tz,)
+    tz_bbox <- sp::bbox(tz)
+    bbox <- bboxToVector(tz_bbox)
+  }
+  
+  # ---- If lat/lon given, Pad them to create a bbox ---------------------------
+  if ( !is.null(latitude) && !is.null(longitude)) {
+    # Pad to roughly 10km. This is ugly, but serves the purpose.
+    bbox <- bboxToVector( c(longitude, longitude + .1, latitude, latitude + .1))
+  }
+  
+  # ALL CALCULATIONS RELY ON THESE VARIABLES FOR BBOX-DERIVED EPHEMERIS. EVEN IF
+  # A BBOX IS NOT PASSED IN, WE MAKE ONE IN ORDER TO CALCULATE DAYLIGHT VALUES
   mid_lon <- NULL
   mid_lat <- NULL
   w <- NULL
@@ -52,9 +97,9 @@ goesaodc_getDaylightHours <- function(
   s <- NULL
   n <- NULL
   
+  # ---- Unwind the bbox (THIS MAY NEED TO MOVE DOWN IN THE FUNCTION) ----------
   if ( !is.null(bbox) ) {
     boundaries <- bboxToVector(bbox)
-    print(boundaries)
     w <- boundaries[1]
     e <- boundaries[2]
     s <- boundaries[3]
@@ -63,27 +108,30 @@ goesaodc_getDaylightHours <- function(
     mid_lon <- w + (e - w) / 2
     mid_lat <- s + (n - s) / 2
   }
-  print(mid_lon)
-  print(mid_lat)
+  
   # ---- Figure out Timezone info, from whatever we have -----------------------
   if ( is.null(timezone) && is.null(bbox) ) {
-    MazamaCoreUtils::stopIfNull(latitude, "Can't calculate timezone. Either pass
-                                 in a valid timezone, or provide a latitude and 
-                                longitude or a bbox.")
+    # Infer timezone using lat/lon
+    MazamaCoreUtils::stopIfNull(latitude, 
+    "Can't calculate timezone. Need timezone, lat/lon, or  bbox.")
     timezone <- MazamaSpatialUtils::getTimezone(lon = longitude, 
                                                 lat = latitude, 
                                                 countryCodes = c("US"))
-    
+    # Infer timezone from bbox center
   } else if ( is.null(timezone) && !is.null(bbox) ) {
     timezone <- MazamaSpatialUtils::getTimezone(lon = mid_lon, 
                                                 lat = mid_lat, 
                                                 countryCodes = c("US"))
   }
   
-  # ---- Ensure we have a POSIXt datetime 
-  datetime <- MazamaCoreUtils::parseDatetime(datetime, timezone, julian = julian)
+  # ---- Ensure we have a POSIXt datetime --------------------------------------
+  datetime <- MazamaCoreUtils::parseDatetime(datetime = datetime, 
+                                             timezone = timezone, 
+                                             julian = julian)
   
-  # USE CASE 2 - BY BBOX (EVERYTHING USES THIS)
+  
+  
+  # ---- CALCULATE EPHEMERIS BY BBOX -------------------------------------------
   west_edge <- matrix(c(w, mid_lat), nrow = 1)
   east_edge <- matrix(c(e, mid_lat), nrow = 1)
   
--- tests/testthat/test-goesaodc_getdaylighthours.R
+++ tests/testthat/test-goesaodc_getdaylighthours.R
@@ -2,28 +2,107 @@ context("test-goesaodc_getdaylighthours")
 
 test_that("Correct daylight sunrise and sunset hours are calculated", {
   
-  # ---- TEST TIMES BASED ON LAT/LON AND DATE ----------------------------------
-  # TEST_DATA
+  library(MazamaSpatialUtils)
+  
+  # ---- TEST_SETUP ------------------------------------------------------------
   sunrise_ll <- MazamaCoreUtils::parseDatetime("2019-09-06 06:42:52", 
                                                timezone = "America/Los_Angeles")
   sunset_ll <- MazamaCoreUtils::parseDatetime("2019-09-06 19:39:02", 
                                               timezone = "America/Los_Angeles")
   ll_dayInfo <- list("sunrise" = sunrise_ll, "sunset" = sunset_ll)
   
-  # GET DAYLIGHT HOURS WITH LAT/LON
+  posix_t <- MazamaCoreUtils::parseDatetime("2019-09-06 16", 
+                                            "America/Los_Angeles")
+  
+  # ---- GET DAYLIGHT HOURS WITH LAT/LON ---------------------------------------
   ll_test_data <- goesaodc_getDaylightHours(datetime = "2019-09-06", 
                                             longitude = -123.245, 
                                             latitude = 42.861)
-  # CALCULATE TIME DIFF
+  
+  # ---- GET DAYLIGHT HOURS WITH BBOX ------------------------------------------
+  bb_test_data <- goesaodc_getDaylightHours(datetime = "2019-09-06", 
+                                            bbox = c(-124.566, -116.463, 
+                                                     41.991, 46.292))
+  
+  # ---- GET DAYLIGHT HOURS WITH DATETIME AND TIMEZONE -------------------------
+  tz_test_data <- goesaodc_getDaylightHours(datetime = "2019-09-06", 
+                                            timezone = "America/Los_Angeles")
+  
+  # --- GET DAYLIGHT HOURS WITH POSIXt ALONE -----------------------------------
+  posix_test_data <- goesaodc_getDaylightHours(posix_t)
+  
+  # ---- LAT/LON TIME DIFFS ----------------------------------------------------
   ll_sunrise_diff <- as.numeric(difftime(ll_dayInfo$sunrise,
                                          ll_test_data$sunrise,
-                                         units = "secs"))
+                                         units = "mins"))
                                          
                                 
   ll_sunset_diff <- as.numeric(difftime(ll_dayInfo$sunset, 
                                         ll_test_data$sunset, 
-                                        units = "secs"))
-  # ASSERTION
-  expect_lt(ll_sunrise_diff, 1)
-  expect_lt(ll_sunset_diff, 1)
+                                        units = "mins"))
+  
+  # ---- BBOX TIME DIFFS -------------------------------------------------------
+  bb_sunrise_diff <- as.numeric(difftime(ll_dayInfo$sunrise,
+                                         ll_test_data$sunrise,
+                                         units = "mins"))
+  
+  bb_sunset_diff <- as.numeric(difftime(ll_dayInfo$sunset,
+                                         ll_test_data$sunset,
+                                         units = "mins"))
+  
+  # ---- TZ TIME DIFFS ---------------------------------------------------------
+  tz_sunrise_diff <- as.numeric(difftime(ll_dayInfo$sunrise,
+                                         tz_test_data$sunrise,
+                                         units = "mins"))
+  
+  tz_sunset_diff <- as.numeric(difftime(ll_dayInfo$sunset,
+                                         tz_test_data$sunset,
+                                         units = "mins"))
+
+  # ---- POSIX TIME DIFF (SHOULD BE EQUAL TO TZ TIME ---------------------------                                         
+  posix_sunrise_diff <- as.numeric(difftime(tz_test_data$sunrise,
+                                           posix_test_data$sunrise,
+                                           units = "secs"))
+  
+  posix_sunset_diff <- as.numeric(difftime(tz_test_data$sunset,
+                                           posix_test_data$sunset,
+                                           units = "secs"))
+  # ---- LAT/LON ASSERTIONS ----------------------------------------------------
+  expect_lt(ll_sunrise_diff, 60)
+  expect_lt(ll_sunset_diff, 60)
+  
+  # ---- BBOX ASSERTIONS -------------------------------------------------------
+  expect_lt(bb_sunrise_diff, 60)
+  expect_lt(bb_sunset_diff, 60)
+  
+  # ---- TIMEZONE ASSERTIONS ---------------------------------------------------
+  expect_lt(tz_sunrise_diff, 60)
+  expect_lt(tz_sunset_diff, 60)
+  
+  # ---- POSIXt ASSERTIONS -----------------------------------------------------
+  expect_equal(posix_sunrise_diff, 0)
+  expect_equal(posix_sunset_diff, 0)  
+})
+
+test_that("function fails when passed incorrect parameters", {
+  
+  # No datetime given
+  expect_error(
+    goesaodc_getDaylightHours(),
+    regexp = "argument 'datetime' must not be NULL."
+  )
+  
+  # Too much geographic data given
+  expect_error(
+    goesaodc_getDaylightHours(datetime = "2019-09-06", 
+                              bbox = c(0,0,0,0), 
+                              latitude = 0, 
+                              longitude = 0),
+    regexp = "Cannot process both BBOX and Lat/Lon at the same time. Pick one."
+  )
+  
+  # No data available to derive timezone
+  expect_error(
+    goesaodc_getDaylightHours(datetime = "2019-09-06"),
+    regexp = "Can't calculate timezone. Need timezone, lat/lon, or  bbox.")
 })
