87aa223811e723ba5599d3e86f36ff6be61d89c2
Tate Brasel <tatebrasel@Tahoma.local>
2021-05-07 13:28:01 -0700

    feat: added 'goesaodc_calcTrendScanPoints()' function.

---
+++ R/goesaodc_calcTrendScanPoints.R
@@ -0,0 +1,67 @@
+#' @export
+#' 
+#' @title Calculate the trend of a series of GOES scan points
+#' 
+#' @description Creates a \code{SpatialPointsDataFrame} of AOD reading trends 
+#' from a \code{list} of GOES scan \code{SpatialPointsDataFrame}s. A trend value
+#' for a point is calculated by taking the difference between it's average value
+#' in the first half of the scan series and it's average value in the second.
+#' 
+#' @param spdfList A \code{list} of GOES scan \code{SpatialPointsDataFrame}s.
+#' @param na.rm Logical flag whether to remove NA values before calculating the
+#' trend. Defaults to \code{FALSE}.
+#' 
+#' @examples
+#' \donttest{
+#' library(MazamaSatelliteUtils)
+#' setSatelliteDataDir("~/Data/Satellite")
+#' 
+#' bboxOregon <- c(-125, -116, 42, 47)
+#' 
+#' scanPointsList <- goesaodc_createScanPoints(
+#'   satID = "G17",
+#'   datetime = "2020-09-08 12:00",
+#'   endtime = "2020-09-08 13:00",
+#'   timezone = "America/Los_Angeles",
+#'   bbox = bboxOregon
+#' )
+#' 
+#' goesaodc_calcTrendScanPoints(
+#'   spdfList = scanPointsList,
+#'   na.rm = FALSE
+#' )
+#' }
+
+goesaodc_calcTrendScanPoints <- function(
+  spdfList = NULL,
+  na.rm = FALSE
+) {
+  
+  middleScanIndex <- floor(length(spdfList) / 2)
+  
+  half1Avg <- goesaodc_calcAverageScanPoints(
+    spdfList[1:middleScanIndex],
+    na.rm = na.rm
+  )$AOD
+  
+  half2Avg <- goesaodc_calcAverageScanPoints(
+    spdfList[(middleScanIndex + 1):length(spdfList)],
+    na.rm = na.rm
+  )$AOD
+  
+  trendValues <- 
+    tibble::tibble(half1Avg, half2Avg) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(trend = half2Avg - half1Avg) %>%
+    dplyr::pull(trend)
+  
+  trendSpdf <- sp::SpatialPointsDataFrame(
+    coords = scanPointsList[[1]]@coords,
+    data = data.frame(
+      AOD = trendValues
+    )
+  )
+  
+  return(trendSpdf)
+  
+}
